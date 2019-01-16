d739dc95d6ed2930317a6fc6aab63a65a58d93ad
lostredakos <e.giannakosian@gmail.com>
2019-01-16 19:44:17 +0200

    Commit all original files after spring setup

---
+++ src/main/java/com/dataverse/demo/DataverseTestApplication.java
@@ -0,0 +1,14 @@
+package com.dataverse.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataverseTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataverseTestApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/dataverse/demo/DataverseTestApplicationTests.java
@@ -0,0 +1,17 @@
+package com.dataverse.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DataverseTestApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
